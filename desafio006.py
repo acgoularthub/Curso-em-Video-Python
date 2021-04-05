301b6514ad700f0fd874fc17fe8527011b15d42b
Anderson Coleho Goulart <acgoulartmail@gmail.com>
2021-04-05 16:21:23 -0300

    Adicionando correção do código do exercício 006

--- desafio006.py
+++ desafio006.py
@@ -1,2 +1,2 @@
 n = int(input('Digite um número: '))
-print('O dobro de {} é {}, seu triplo é {} e sua raiz quadrada é {}!'.format(n, n*2, n*3, n*(1/2)))
+print('O dobro de {} é {}, seu triplo é {} e sua raiz quadrada é {}!'.format(n, n*2, n*3, n**(1/2)))
